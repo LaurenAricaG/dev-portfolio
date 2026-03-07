64767c07405b7721207f292dc42c1f1dc41edfb0
LaurenAricaG <laurendavid159@gmail.com>
2026-03-07 13:41:39 -0500

    feat(projects): add project cards with dynamic data, tech config and types

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,21 @@
 import Container from "@/components/layout/Container";
+import CardProject from "@/components/ProjectsSection/CardProject";
 import Title from "@/components/Tittle";
+import { Project } from "@/types/projects.type";
+import projectsData from "@/data/projects-data.json";
 
 const ProjectsPage = () => {
+  const projects = projectsData.projects as Project[];
+
   return (
     <section>
       <Container className="mt-5 md:mt-10">
         <Title label="Portafolio" title="Mis" subtitle="Proyectos" level="h1" />
-        <div className="flex flex-col gap-16 mt-12">Mapear los projectos</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+          {projects.map((project) => (
+            <CardProject key={project.id} project={project} />
+          ))}
+        </div>
       </Container>
     </section>
   );
---
+++ types/projects.type.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  technologies: string[];
+  githubUrl: string;
+  liveUrl: string;
+  imageUrl: string;
+  type?: "FrontEnd" | "BackEnd" | "FullStack";
+}
